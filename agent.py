5a2743988e3b9cf5cbe90e421aea6a033213c05f
Jonathan Viquerat <jonathan.viquerat@mines-paristech.fr>
2020-08-12 14:56:13 +0200

    Good

--- agent.py
+++ agent.py
@@ -15,6 +15,7 @@ from   tensorflow.keras.initializers import Orthogonal
 
 # Define alias
 tfd = tfp.distributions
+tf.random.set_seed(1)
 
 ###############################################
 ### PPO actor
