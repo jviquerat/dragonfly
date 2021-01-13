b1286d77a07a20ee18917aefa1fc1c7ee1bafc97
jviquerat <jonathan.viquerat@mines-paristech.fr>
2021-01-13 07:48:46 +0100

    Added main line

--- start.py
+++ start.py
@@ -19,58 +19,60 @@ def params_decoder(p_dict):
 # Average training over multiple runs
 ########################
 
-# Check command-line input for json file
-if (len(sys.argv) == 2):
-    json_file = sys.argv[1]
-else:
-    print('Command line error, please use as follows:')
-    print('python3 start.py my_file.json')
+if __name__ == '__main__':
 
-# Read json parameter file
-with open(json_file, "r") as f:
-    params = json.load(f, object_hook=params_decoder)
+    # Check command-line input for json file
+    if (len(sys.argv) == 2):
+        json_file = sys.argv[1]
+    else:
+        print('Command line error, please use as follows:')
+        print('python3 start.py my_file.json')
 
-# Storage arrays
-res_path   = 'results'
-n_data     = 9
-ep         = np.zeros((              params.n_ep),           dtype=int)
-data       = np.zeros((params.n_avg, params.n_ep,   n_data), dtype=float)
-avg_data   = np.zeros((              params.n_ep,   n_data), dtype=float)
-stdp_data  = np.zeros((              params.n_ep,   n_data), dtype=float)
-stdm_data  = np.zeros((              params.n_ep,   n_data), dtype=float)
+    # Read json parameter file
+    with open(json_file, "r") as f:
+        params = json.load(f, object_hook=params_decoder)
 
-# Open storage repositories
-if (not os.path.exists(res_path)):
-    os.makedirs(res_path)
+    # Storage arrays
+    res_path   = 'results'
+    n_data     = 9
+    ep         = np.zeros((              params.n_ep),           dtype=int)
+    data       = np.zeros((params.n_avg, params.n_ep,   n_data), dtype=float)
+    avg_data   = np.zeros((              params.n_ep,   n_data), dtype=float)
+    stdp_data  = np.zeros((              params.n_ep,   n_data), dtype=float)
+    stdm_data  = np.zeros((              params.n_ep,   n_data), dtype=float)
 
-t         = time.localtime()
-path_time = time.strftime("%H-%M-%S", t)
-path      = res_path+'/'+params.env_name+'_'+str(path_time)
-if (not os.path.exists(path)):
-    os.makedirs(path)
+    # Open storage repositories
+    if (not os.path.exists(res_path)):
+        os.makedirs(res_path)
 
-for i in range(params.n_avg):
-    print('### Avg run #'+str(i))
-    start_time = time.time()
-    launch_training(params, path, i)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    t         = time.localtime()
+    path_time = time.strftime("%H-%M-%S", t)
+    path      = res_path+'/'+params.env_name+'_'+str(path_time)
+    if (not os.path.exists(path)):
+        os.makedirs(path)
 
-    f           = np.loadtxt(path+'/ppo.dat')
-    ep          = f[:params.n_ep,0]
+    for i in range(params.n_avg):
+        print('### Avg run #'+str(i))
+        start_time = time.time()
+        launch_training(params, path, i)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+        f           = np.loadtxt(path+'/ppo.dat')
+        ep          = f[:params.n_ep,0]
+        for j in range(n_data):
+            data[i,:,j] = f[:params.n_ep,j+1]
+
+    # Write to file
+    file_out  = path+'/ppo_avg.dat'
+    array     = np.vstack(ep)
     for j in range(n_data):
-        data[i,:,j] = f[:params.n_ep,j+1]
+        avg   = np.mean(data[:,:,j], axis=0)
+        std   = np.std (data[:,:,j], axis=0)
+        p     = avg + std
+        m     = avg - std
+        array = np.hstack((array,np.vstack(avg)))
+        array = np.hstack((array,np.vstack(p)))
+        array = np.hstack((array,np.vstack(m)))
 
-# Write to file
-file_out  = path+'/ppo_avg.dat'
-array     = np.vstack(ep)
-for j in range(n_data):
-    avg   = np.mean(data[:,:,j], axis=0)
-    std   = np.std (data[:,:,j], axis=0)
-    p     = avg + std
-    m     = avg - std
-    array = np.hstack((array,np.vstack(avg)))
-    array = np.hstack((array,np.vstack(p)))
-    array = np.hstack((array,np.vstack(m)))
-
-np.savetxt(file_out, array, fmt='%.5e')
-os.system('gnuplot -c plot/plot.gnu '+path)
+    np.savetxt(file_out, array, fmt='%.5e')
+    os.system('gnuplot -c plot/plot.gnu '+path)
