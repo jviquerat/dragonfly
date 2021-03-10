be96223addd14a686844d525dc9b6154019f732d
jviquerat <jonathan.viquerat@mines-paristech.fr>
2021-03-10 11:12:40 +0100

    Cleaning

--- dragonfly/core/training.py
+++ dragonfly/core/training.py
@@ -14,11 +14,8 @@ def launch_training(params, path, run):
     env   = par_envs(params.env_name, params.n_cpu, path)
     agent = ppo(env.act_dim, env.obs_dim, params)
 
-    # Initialize parameters
-    #ep      =  0
-    #ep_step = [0     for _ in range(params.n_cpu)]
-    #score   = [0.0   for _ in range(params.n_cpu)]
-    obs     = env.reset()
+    # Reset environment
+    obs = env.reset()
 
     # Loop until max episode number is reached
     while (agent.test_ep_loop()):
@@ -40,11 +37,9 @@ def launch_training(params, path, run):
             agent.store_transition(obs, nxt, act, rwd, trm)
 
             # Update observation and buffer counter
-            obs       = nxt
+            obs = nxt
             agent.update_score(rwd)
             agent.update_step()
-            #score[:] += rwd[:]
-            #ep_step   = [x+1 for x in ep_step]
 
             # Handle rendering
             rnd = env.render(agent.get_render_cpu())
@@ -63,12 +58,9 @@ def launch_training(params, path, run):
                     # Handle rendering
                     agent.finish_rendering(path, cpu)
 
-                    # Reset
+                    # Reset environment and counters
                     obs[cpu] = env.reset_single(cpu)
                     agent.reset_ep(cpu)
-                    #score[cpu]   = 0
-                    #ep_step[cpu] = 0
-                    #ep          += 1
 
         # Train networks
         agent.train_networks()
