3b10444cb9a118106fe2544416c02e3bf4584d26
jviquerat <jonathan.viquerat@mines-paristech.fr>
2021-04-16 11:44:45 +0200

    Started working on normal policy, not finished

--- dragonfly/policy/normal.py
+++ dragonfly/policy/normal.py
@@ -13,8 +13,49 @@ from dragonfly.optimizer.optimizer import *
 class normal():
     def __init__(self, obs_dim, act_dim, pms):
 
-        # Dimension of policy output
-        self.dim  = 2*act_dim
+        # Set default values
+        self.save = False
+
+        # Check inputs
+        if hasattr(pms, "save"): self.save = pms.save
+
+        # Fill structure
+        self.act_dim = act_dim
+        self.obs_dim = obs_dim
+        self.dim     = self.act_dim
+
+        # Define and init mu network
+        # Force tanh activation, as this is normal policy
+        pms.mu_network.fnl_actv = "tanh"
+        self.net_mu = net_factory.create(pms.mu_network.type,
+                                         inp_dim = obs_dim,
+                                         out_dim = self.dim,
+                                         pms     = pms.mu_network)
+
+        # Define and init sg network
+        # Force sigmoid activation, as this is normal policy
+        pms.sg_network.fnl_actv = "sigmoid"
+        self.net_sg = net_factory.create(pms.sg_network.type,
+                                         inp_dim = obs_dim,
+                                         out_dim = self.dim,
+                                         pms     = pms.sg_network)
+
+        # Define optimizers
+        self.mu_opt = opt_factory.create(pms.optimizer.type,
+                                         pms       = pms.optimizer,
+                                         grad_vars = self.mu_net.trainable_weights)
+
+        self.sg_opt = opt_factory.create(pms.optimizer.type,
+                                         pms       = pms.optimizer,
+                                         grad_vars = self.sg_net.trainable_weights)
+
+        # Define loss
+        self.loss = loss_factory.create(pms.loss.type,
+                                        pms = pms.loss)
+
+        # Optional previous version of network
+        if (self.save):
+            self.prv = cp(self.net)
 
     # Call policy
     def call(self, params):
