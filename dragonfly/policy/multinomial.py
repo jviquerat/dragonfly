55f6106b50b6fc6d9586afd276f293b96079c8df
jviquerat <jonathan.viquerat@mines-paristech.fr>
2021-04-21 09:32:49 +0200

    Modifying multinomial to tf

--- dragonfly/policy/multinomial.py
+++ dragonfly/policy/multinomial.py
@@ -1,20 +1,9 @@
 # Generic imports
-import os
-import warnings
-import numpy                  as np
-from   copy import deepcopy   as cp
-
-# Import tensorflow and filter warning messages
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '10'
-warnings.filterwarnings('ignore',category=FutureWarning)
-import tensorflow             as tf
-import tensorflow_probability as tfp
-
-# Define alias
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-tfd = tfp.distributions
+import numpy                as np
+from   copy import deepcopy as cp
 
 # Custom imports
+from dragonfly.policy.tfd          import *
 from dragonfly.network.network     import *
 from dragonfly.optimizer.optimizer import *
 from dragonfly.loss.loss           import *
