d2725c41d27dd0b767ca280a47fefce34745bb3e
jviquerat <jonathan.viquerat@mines-paristech.fr>
2021-04-22 14:41:46 +0200

    Added pg loss

--- dragonfly/loss/loss.py
+++ dragonfly/loss/loss.py
@@ -2,6 +2,7 @@
 from dragonfly.core.factory import *
 from dragonfly.loss.mse     import *
 from dragonfly.loss.ppo     import *
+from dragonfly.loss.pg      import *
 
 # Declare factory
 loss_factory = factory()
@@ -9,3 +10,4 @@ loss_factory = factory()
 # Register values
 loss_factory.register("mse", mse)
 loss_factory.register("ppo", ppo)
+loss_factory.register("pg",  pg)
---
+++ dragonfly/loss/pg.py
@@ -0,0 +1,48 @@
+# Tensorflow imports
+import tensorflow as tf
+
+# Custom imports
+from dragonfly.core.constants import *
+
+###############################################
+### PG loss class
+class pg():
+    def __init__(self, pms):
+
+        # Set default values
+        self.ent_coef   = 0.01
+
+        # Check inputs
+        if hasattr(pms, "ent_coef"): self.ent_coef = pms.ent_coef
+
+    # Train
+    @tf.function
+    def train(self, obs, adv, act, policy):
+        with tf.GradientTape() as tape:
+
+            # Compute loss
+            pdf     = policy.compute_pdf(obs)
+            act     = tf.reshape(act, [-1])
+            lgp     = pdf.log_prob(act)
+            loss_pg = -tf.reduce_mean(lgp)
+
+            # Compute entropy loss
+            entropy      = pdf.entropy()
+            entropy      = tf.reshape(entropy, [-1])
+            entropy      = tf.reduce_mean(entropy, axis=0)
+            loss_entropy =-entropy
+
+            # Compute total loss
+            loss = loss_pg + self.ent_coef*loss_entropy
+
+            # Compute KL div
+            kl = pdf.kl_divergence(prp)
+            kl = tf.reduce_mean(kl)
+
+            # Apply gradients
+            pol_var = policy.net.trainable_variables
+            grads   = tape.gradient(loss, pol_var)
+            norm    = tf.linalg.global_norm(grads)
+        policy.opt.apply_grads(zip(grads, pol_var))
+
+        return loss, kl, norm, entropy
