7605860307f605cd6e153aab38dc77df6b72c363
jviquerat <jonathan.viquerat@mines-paristech.fr>
2021-07-13 12:34:09 +0200

    Set timers in training

--- dragonfly/core/training.py
+++ dragonfly/core/training.py
@@ -10,9 +10,10 @@ from dragonfly.utils.timer import *
 def launch_training(path, run, env, agent):
 
     # Initialize timers
-    timer_global   = timer("global  ")
-    timer_training = timer("training")
-    timer_env      = timer("env     ")
+    timer_global   = timer("global   ")
+    timer_env      = timer("env      ")
+    timer_actions  = timer("actions  ")
+    timer_training = timer("training ")
 
     # Start global timer
     timer_global.tic()
@@ -29,24 +30,26 @@ def launch_training(path, run, env, agent):
         # Loop over buff size
         while (agent.test_buff_loop()):
 
-            # Make one iteration
-            act            = agent.get_actions(obs)
+            # Get actions
+            timer_actions.tic()
+            act = agent.get_actions(obs)
+            timer_actions.toc()
+
+            # Make one env step
             timer_env.tic()
             nxt, rwd, done = env.step(act)
             timer_env.toc()
 
             # Handle termination state
-            trm, done      = agent.handle_term(done)
+            trm, done = agent.handle_term(done)
 
             # Store transition
             agent.store_transition(obs, nxt, act, rwd, trm)
 
             # Update observation and buffer counter
             obs = nxt
-            timer_training.tic()
             agent.update_score(rwd)
             agent.update_step()
-            timer_training.toc()
 
             # Handle rendering
             rnd = env.render(agent.get_render_cpu())
@@ -60,9 +63,13 @@ def launch_training(path, run, env, agent):
             env.reset(done, obs)
             timer_env.toc()
 
-        # Prepare training and train agent
+        # Finalize buffers for training
         agent.finalize_buffers()
+
+        # Train agent
+        timer_training.tic()
         agent.train()
+        timer_training.toc()
 
         # Write report data to file
         agent.write_report(path, run)
@@ -74,4 +81,5 @@ def launch_training(path, run, env, agent):
     timer_global.toc()
     timer_global.show()
     timer_env.show()
+    timer_actions.show()
     timer_training.show()
