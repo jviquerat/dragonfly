0af30830b872a5fb4331b72d52527e1a7ccc17a7
jviquerat <jonathan.viquerat@mines-paristech.fr>
2022-01-13 16:10:38 +0100

    Added terminator

---
+++ dragonfly/src/terminator/regular.py
@@ -0,0 +1,33 @@
+# Generic imports
+import numpy as np
+
+###############################################
+### Class for regular terminator
+### pms : parameters
+class regular():
+    def __init__(self, n_cpu, pms):
+
+        # Set values
+        self.n_cpu = n_cpu
+
+    # Terminate buffers
+    def terminate(self, buff):
+
+        # Initialize arrays
+        trm = np.zeros([self.n_cpu])
+        bts = np.zeros([self.n_cpu])
+
+        # Loop over steps
+        for k in range(buff.length()):
+
+            trm[:] = 0.0
+            bts[:] = 0.0
+
+            # Loop over environments
+            for i in range(self.n_cpu):
+
+                # Set terminal value, whatever the cause
+                trm[i] = float(not (buff.data["dne"].buff[i][k] == True))
+
+            # Store terminal data to buffer
+            buff.store(["trm", "bts"], [trm, bts])
