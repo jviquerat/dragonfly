db08d43f663884fdb6290a5d2dedcd0bb284c38a
jviquerat <jonathan.viquerat@mines-paristech.fr>
2022-12-22 13:44:03 +0100

    Fixed sac reparameterization trick

--- dragonfly/src/policy/tanh_normal.py
+++ dragonfly/src/policy/tanh_normal.py
@@ -10,13 +10,25 @@ class tanh_normal(normal):
 
         super().__init__(obs_dim, act_dim, pms)
 
-    # Compute pdf
-    def compute_pdf(self, obs):
+    # Sample actions
+    @tf.function
+    def sample(self, obs):
 
-        # Get pdf
-        mu, sg = self.forward(obs)
-        nrm    = tfd.MultivariateNormalDiag(loc        = mu,
-                                            scale_diag = sg)
-        pdf    = tfd.TransformedDistribution(nrm, tfp.bijectors.Tanh())
+        # Reparameterization trick
+        mu, sg   = self.forward(obs)
+        act      = mu + tf.random.normal(tf.shape(mu))*sg
+        act      = tf.reshape(act, [-1,self.store_dim])
+        tanh_act = tf.tanh(act)
 
-        return pdf
+        lgp =-0.5*(((act - mu)/(sg + 1.0e-8))**2 + 2.0*tf.math.log(sg) + tf.math.log(2.0*np.pi))
+        lgp = tf.reduce_sum(lgp, axis=1)
+        lgp = tf.reshape(lgp, [-1,1])
+
+        # Log-prob of reparameterized action
+        #sth = tf.math.log(1.0 - tf.square(tanh_act) + 1.0e-8)
+        sth  = 2.0*(np.log(2.0) - act - tf.nn.softplus(-2.0*act)) # from openai implementation
+        sth  = tf.reduce_sum(sth, axis=1)
+        sth  = tf.reshape(sth, [-1,1])
+        lgp -= sth
+
+        return tanh_act, lgp
