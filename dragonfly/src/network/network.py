dddaab7c8f190817771c3a31864863a97fe5f583
Jonathan Viquerat <jonathan.viquerat@mines-paristech.fr>
2023-01-16 09:12:24 +0100

    Added snn (#261)

--- dragonfly/src/network/network.py
+++ dragonfly/src/network/network.py
@@ -1,9 +1,11 @@
 # Custom imports
 from dragonfly.src.core.factory  import *
 from dragonfly.src.network.fc    import *
+from dragonfly.src.network.snn   import *
 
 # Declare factory
 net_factory = factory()
 
 # Register values
-net_factory.register("fc", fc)
+net_factory.register("fc",  fc)
+net_factory.register("snn", snn)
