a5a6a6ef465f57f2779d69fb56eb6566f73be065
jviquerat <jonathan.viquerat@mines-paristech.fr>
2023-03-28 10:55:03 +0200

    Removed unused optimizers

--- dragonfly/src/optimizer/optimizer.py
+++ dragonfly/src/optimizer/optimizer.py
@@ -2,10 +2,7 @@
 from dragonfly.src.core.factory       import *
 from dragonfly.src.optimizer.nadam    import *
 from dragonfly.src.optimizer.adam     import *
-from dragonfly.src.optimizer.adadelta import *
-from dragonfly.src.optimizer.adamax   import *
 from dragonfly.src.optimizer.sgd      import *
-from dragonfly.src.optimizer.ftrl     import *
 
 # Declare factory
 opt_factory = factory()
@@ -13,7 +10,4 @@ opt_factory = factory()
 # Register values
 opt_factory.register("nadam",    nadam)
 opt_factory.register("adam",     adam)
-opt_factory.register("adadelta", adadelta)
-opt_factory.register("adamax",   adamax)
 opt_factory.register("sgd",      sgd)
-opt_factory.register("ftrl",     ftrl)
