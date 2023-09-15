6b881f75a8b8a811d4aad641d9caf5174023d625
Amir-HB <ahbateni@gmail.com>
2023-09-15 09:57:19 +0200

    Added normal policies with constant and isotropic variance

--- dragonfly/src/policy/policy.py
+++ dragonfly/src/policy/policy.py
@@ -6,6 +6,8 @@ from dragonfly.src.policy.normal_full    import *
 from dragonfly.src.policy.tanh_normal    import *
 from dragonfly.src.policy.beta           import *
 from dragonfly.src.policy.deterministic  import *
+from dragonfly.src.policy.normal_iso     import *
+from dragonfly.src.policy.normal_const   import *
 
 # Declare factory
 pol_factory = factory()
@@ -18,4 +20,5 @@ pol_factory.register("normal_full",    normal_full)
 pol_factory.register("tanh_normal",    tanh_normal)
 pol_factory.register("beta",           beta)
 pol_factory.register("deterministic",  deterministic)
-
+pol_factory.register("normal_iso",     normal_iso)
+pol_factory.register("normal_const",   normal_const)
---
+++ dragonfly/tst/policy/test_normal_iso.py
@@ -0,0 +1,36 @@
+# Generic imports
+import pytest
+
+# Custom imports
+from dragonfly.tst.tst               import *
+from dragonfly.src.policy.normal_iso import *
+from dragonfly.src.utils.json        import *
+
+###############################################
+### Test isotropic normal policy
+def test_normal_iso():
+
+    # Initial space
+    print("")
+
+    #########################
+    # Initialize json parser and read test json file
+    reader = json_parser()
+    reader.read("dragonfly/tst/policy/normal_iso.json")
+
+    # Initialize discrete agent
+    policy = normal_iso(1, 5, reader.pms.policy)
+
+    # Test action values
+    print("Test isotropic normal policy")
+    obs = [[1.0]]
+    act, lgp = policy.actions(obs)
+    print("Actions:",act)
+
+    obs = tf.cast([obs], tf.float32)
+    mu, sg = policy.forward(obs)
+    assert(np.all(np.abs(mu) < 1.0))
+    assert(np.all(np.abs(sg) < 1.0))
+    assert(np.all(np.abs(sg) > 0.0))
+
+    print("")
