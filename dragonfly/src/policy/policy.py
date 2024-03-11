22b05bac50f47e5127a02ef125a1c90b2067d0df
jviquerat <jonathan.viquerat@mines-paristech.fr>
2024-03-11 15:05:06 +0100

    Removed beta policy

--- dragonfly/src/policy/policy.py
+++ dragonfly/src/policy/policy.py
@@ -4,7 +4,6 @@ from dragonfly.src.policy.categorical    import *
 from dragonfly.src.policy.normal_diag    import *
 from dragonfly.src.policy.normal_full    import *
 from dragonfly.src.policy.tanh_normal    import *
-from dragonfly.src.policy.beta           import *
 from dragonfly.src.policy.deterministic  import *
 from dragonfly.src.policy.normal_iso     import *
 from dragonfly.src.policy.normal_const   import *
@@ -18,7 +17,6 @@ pol_factory.register("normal",         normal_diag)
 pol_factory.register("normal_diag",    normal_diag)
 pol_factory.register("normal_full",    normal_full)
 pol_factory.register("tanh_normal",    tanh_normal)
-pol_factory.register("beta",           beta)
 pol_factory.register("deterministic",  deterministic)
 pol_factory.register("normal_iso",     normal_iso)
 pol_factory.register("normal_const",   normal_const)
