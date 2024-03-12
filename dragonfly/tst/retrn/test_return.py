9183017fb9867c8176ab22d453499087c9b5c92a
jviquerat <jonathan.viquerat@mines-paristech.fr>
2024-03-12 11:23:26 +0100

    Fixed GAE implementation and added more tests

--- dragonfly/tst/retrn/test_return.py
+++ dragonfly/tst/retrn/test_return.py
@@ -20,12 +20,10 @@ def test_return():
     rwd  = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
     val  = np.array([4.0, 3.0, 2.0, 1.0, 0.5])
     nxt  = np.array([3.0, 2.0, 1.0, 0.5, 0.2])
-    trm  = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
-    bts  = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
 
     #########################
     # Test discounted return without bootstrap
-    print("Disc. return, no norm, no clip, no bootstrap, no terminal")
+    print("Disc. return, no norm, no bootstrap, no terminal")
 
     # Read json file and declare return
     reader = json_parser()
@@ -33,6 +31,7 @@ def test_return():
     retrn = full(reader.pms.retrn)
 
     # Compute returns
+    trm      = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
     tgt, ret = retrn.compute(rwd, val, nxt, trm)
     ret_ref  = np.array([4.90099501, 3.940399, 2.9701, 1.99, 1.0])
 
@@ -46,12 +45,10 @@ def test_return():
 
     #########################
     # Test discounted return with terminal state
-    print("Disc. return, no norm, no clip, no bootstrap, with terminal")
-
-    # Modify trm vector
-    trm[2] = 0.0
+    print("Disc. return, no norm, no bootstrap, with terminal")
 
     # Compute returns
+    trm      = np.array([1.0, 1.0, 0.0, 1.0, 1.0])
     tgt, ret = retrn.compute(rwd, val, nxt, trm)
     ret_ref  = np.array([ 2.9701, 1.99, 1.0, 1.99, 1.0])
 
@@ -65,12 +62,10 @@ def test_return():
 
     #########################
     # Test discounted return with bootstrap
-    print("Disc. return, no norm, no clip, with bootstrap, with terminal")
-
-    # Modify bts vector
-    trm[2] = 2.0
+    print("Disc. return, no norm, with bootstrap, with terminal")
 
     # Compute returns
+    trm      = np.array([1.0, 1.0, 2.0, 1.0, 1.0])
     tgt, ret = retrn.compute(rwd, val, nxt, trm)
     ret_ref  = np.array([3.940399, 2.9701, 1.99, 1.99, 1.0])
 
@@ -84,10 +79,7 @@ def test_return():
 
     #########################
     # Test discounted return with normalization
-    print("Disc. return, normalized, no clip, no bootstrap, no terminal")
-
-    # Reset trm vector
-    trm[2] = 1.0
+    print("Disc. return, normalized, no bootstrap, no terminal")
 
     # Modify retrn object
     retrn.ret_norm = True
@@ -95,6 +87,7 @@ def test_return():
     # Compute returns
     # avg is 2.960298802
     # std is 1.3792204603784504
+    trm      = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
     tgt, ret = retrn.compute(rwd, val, nxt, trm)
     ret_ref  = np.array([1.407096446,    0.7106189519,
                          0.007106331643,-0.7035124767,-1.421309253])
@@ -109,16 +102,50 @@ def test_return():
 
     #########################
     # Test vanilla advantage
-    print("Advantage, no norm, no clip, no bootstrap, no terminal")
+    print("Advantage, no norm, no bootstrap, no terminal")
 
     # Read json file and declare return
     reader.read("dragonfly/tst/retrn/advantage.json")
     retrn = advantage(reader.pms.retrn)
 
     # Compute returns
+    trm      = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
     tgt, ret = retrn.compute(rwd, val, nxt, trm)
-    ret_ref  = np.array([4.90099501, 3.940399, 2.9701, 1.99, 1.])
-    ret_ref -= val
+    ret_ref  = np.array([0.90099501, 0.940399, 0.9701, 0.99, 0.5])
+
+    print("Reference return")
+    print(ret_ref)
+    print("Computed return")
+    print(ret)
+
+    assert(np.all(np.abs(ret-ret_ref)/np.abs(ret_ref + ret_eps) < 1.0e-8))
+    print("")
+
+    #########################
+    # Test advantage with terminal state
+    print("Advantage, no norm, no bootstrap, with terminal")
+
+    # Compute returns
+    trm      = np.array([1.0, 1.0, 0.0, 1.0, 1.0])
+    tgt, ret = retrn.compute(rwd, val, nxt, trm)
+    ret_ref  = np.array([-1.0299, -1.01, -1.0, 0.99, 0.5])
+
+    print("Reference return")
+    print(ret_ref)
+    print("Computed return")
+    print(ret)
+
+    assert(np.all(np.abs(ret-ret_ref)/np.abs(ret_ref + ret_eps) < 1.0e-8))
+    print("")
+
+    #########################
+    # Test advantage with bootstrap
+    print("Advantage, no norm, with bootstrap, with terminal")
+
+    # Compute returns
+    trm      = np.array([1.0, 1.0, 2.0, 1.0, 1.0])
+    tgt, ret = retrn.compute(rwd, val, nxt, trm)
+    ret_ref  = np.array([-0.059601, -0.0299, -0.01, 0.99, 0.5])
 
     print("Reference return")
     print(ret_ref)
@@ -130,16 +157,50 @@ def test_return():
 
     #########################
     # Test vanilla GAE
-    print("GAE, no norm, no clip, no bootstrap, no terminal")
+    print("GAE, no norm, no bootstrap, no terminal")
 
     # Read json file and declare return
     reader.read("dragonfly/tst/retrn/gae.json")
     retrn = gae(reader.pms.retrn)
 
     # Compute returns
+    trm      = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
     tgt, ret = retrn.compute(rwd, val, nxt, trm)
-    ret_ref  = np.array([0.39323576, 0.43623558,
-                         0.47024900, 0.49500000, 0.0])
+    ret_ref  = np.array([1.01167994, 1.07367546, 1.12726805, 1.1721996, 0.698])
+
+    print("Reference return")
+    print(ret_ref)
+    print("Computed return")
+    print(ret)
+
+    assert(np.all(np.abs(ret-ret_ref)/np.abs(ret_ref + ret_eps) < 1.0e-8))
+    print("")
+
+    #########################
+    # Test GAE with terminal
+    print("GAE, no norm, no bootstrap, with terminal")
+
+    # Compute returns
+    trm      = np.array([1.0, 1.0, 1.0, 1.0, 0.0])
+    tgt, ret = retrn.compute(rwd, val, nxt, trm)
+    ret_ref  = np.array([0.83624735, 0.89285441, 0.94089302, 0.9801, 0.5])
+
+    print("Reference return")
+    print(ret_ref)
+    print("Computed return")
+    print(ret)
+
+    assert(np.all(np.abs(ret-ret_ref)/np.abs(ret_ref + ret_eps) < 1.0e-8))
+    print("")
+
+    #########################
+    # Test GAE with bootstrap
+    print("GAE, no norm, with bootstrap, with terminal")
+
+    # Compute returns
+    trm      = np.array([1.0, 1.0, 2.0, 1.0, 1.0])
+    tgt, ret = retrn.compute(rwd, val, nxt, trm)
+    ret_ref  = np.array([1.01167994, 1.07367546, 1.12726805, 1.1721996, 0.698])
 
     print("Reference return")
     print(ret_ref)
