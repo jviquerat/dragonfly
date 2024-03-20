957126777743823f6f63685d5a5e2796e3557968
amir-hb <ahbateni@gmail.com>
2024-03-20 16:44:26 +0100

    Implemented lstm network with test

--- dragonfly/src/network/network.py
+++ dragonfly/src/network/network.py
@@ -3,6 +3,7 @@ from dragonfly.src.core.factory   import *
 from dragonfly.src.network.fc     import *
 from dragonfly.src.network.d2rl   import *
 from dragonfly.src.network.conv1d import *
+from dragonfly.src.network.lstm   import *
 
 # Declare factory
 net_factory = factory()
@@ -11,3 +12,4 @@ net_factory = factory()
 net_factory.register("fc",     fc)
 net_factory.register("d2rl",   d2rl)
 net_factory.register("conv1d", conv1d)
+net_factory.register("lstm",   lstm)
