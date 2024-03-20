5fb20c5c04fddeebf4361b56fba4356f3314c622
amir-hb <ahbateni@gmail.com>
2024-03-20 16:44:07 +0100

    Added conv1d network with doc and tests

--- dragonfly/src/network/network.py
+++ dragonfly/src/network/network.py
@@ -1,11 +1,13 @@
 # Custom imports
-from dragonfly.src.core.factory  import *
-from dragonfly.src.network.fc    import *
+from dragonfly.src.core.factory   import *
+from dragonfly.src.network.fc     import *
 from dragonfly.src.network.d2rl   import *
+from dragonfly.src.network.conv1d import *
 
 # Declare factory
 net_factory = factory()
 
 # Register values
-net_factory.register("fc",  fc)
-net_factory.register("d2rl", d2rl)
+net_factory.register("fc",     fc)
+net_factory.register("d2rl",   d2rl)
+net_factory.register("conv1d", conv1d)
