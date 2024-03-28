c4ae0c9628f99a75eb450ecc16fd370ec5c38d5b
jviquerat <jonathan.viquerat@mines-paristech.fr>
2024-03-28 14:08:40 +0100

    Added basic shell-based benchmark

---
+++ dragonfly/env/benchmarks/bench_folder.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#######################################################
+# Run all .json scripts contained in a provided folder recursively
+# You need to source virtual environment before starting
+#######################################################
+
+# Retrieve folder name
+input=$1
+
+# Remove possible trailing slash
+folder=${input%/}
+
+# List all json files
+lst=($(find $folder -type f -name '*.json'))
+
+# Print files
+echo "Benchmarking files:"
+echo " "
+for f in ${lst[@]}; do
+    echo $f
+
+    # Remove possible leading characters
+    name=${f#..}
+    name=${name#/}
+
+    # Remove json extension
+    name=${name%.*}
+
+    # Convert slashes to underscore
+    name=${name//\//_}
+
+    # Run with output in dedicated file
+    dgf --train $f &> $name &
+done
