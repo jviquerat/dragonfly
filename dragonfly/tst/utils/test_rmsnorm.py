d2e15650c0651cb9072684edaed6cd8a895a7392
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2024-04-16 15:27:42 +0200

    misc: make test more uniform

--- dragonfly/tst/utils/test_rmsnorm.py
+++ dragonfly/tst/utils/test_rmsnorm.py
@@ -1,34 +1,22 @@
 import tensorflow as tf
-import numpy as np
-import unittest
 
 from dragonfly.src.utils.rmsnorm import *
 
+def test_rmsnorm_shape():
+    model = RMSNorm()
+    input_shape = (4, 5, 3)
+    inputs = tf.ones(input_shape)
+    outputs = model(inputs)
+    assert outputs.shape == input_shape
 
-class TestRMSNorm(unittest.TestCase):
-    def setUp(self):
-        self.model = RMSNorm()
+def test_rmsnorm():
+    model = RMSNorm()
+    input_data = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], dtype=tf.float32)
+    inputs = tf.expand_dims(input_data, axis=0)
 
-    def tearDown(self):
-        self.model = None
-
-    def test_output_shape(self):
-        input_shape = (4, 5, 3)
-        inputs = tf.ones(input_shape)
-        outputs = self.model(inputs)
-        self.assertEqual(outputs.shape, input_shape)
-
-    def test_output_values(self):
-        input_data = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], dtype=tf.float32)
-        inputs = tf.expand_dims(input_data, axis=0)
-
-        expected_output = tf.constant(
-            [[[0.46291006, 0.9258201, 1.3887302], [0.7895421, 0.98692757, 1.184313]]],
-            dtype=tf.float32,
-        )
-        outputs = self.model(inputs)
-        tf.debugging.assert_near(outputs, expected_output, atol=1e-5)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    expected_output = tf.constant(
+        [[[0.46291006, 0.9258201, 1.3887302], [0.7895421, 0.98692757, 1.184313]]],
+        dtype=tf.float32,
+    )
+    outputs = model(inputs)
+    tf.debugging.assert_near(outputs, expected_output, atol=1e-5)
