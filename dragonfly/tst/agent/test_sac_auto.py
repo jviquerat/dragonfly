a356f44e89f76cb0a0a70af947d595f948081456
jviquerat <jonathan.viquerat@mines-paristech.fr>
2024-05-13 10:53:49 +0200

    Added SAC with automatic temperature tuning

---
+++ dragonfly/tst/agent/test_sac_auto.py
@@ -0,0 +1,13 @@
+# Generic imports
+import pytest
+
+# Custom imports
+from dragonfly.tst.tst    import *
+from dragonfly.tst.runner import *
+
+###############################################
+### Test sac_auto agent
+def test_sac_auto():
+
+    runner("dragonfly/tst/agent/sac_auto.json",
+           "sac_auto")
