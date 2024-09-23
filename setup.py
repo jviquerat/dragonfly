7ac15a26291b1abb1d257f54e3152d4577702e9b
jviquerat <jonathan.viquerat@mines-paristech.fr>
2024-09-23 11:26:29 +0200

    Removed dependance to gym in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name='dgf',
     version='0.0.1',
-    install_requires=['gym'],
     entry_points = {
         'console_scripts': ['dgf=dragonfly.src.core.main:main']
     }
