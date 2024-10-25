a0b020f14a84368def6b72dc11c642fefdcbbaff
Jonathan <jonathan.viquerat@mines-paristech.fr>
2024-10-25 16:27:36 +0200

    conv1d: removed

--- dragonfly/src/network/network.py
+++ dragonfly/src/network/network.py
@@ -2,7 +2,6 @@
 from dragonfly.src.core.factory      import factory
 from dragonfly.src.network.fc        import fc
 from dragonfly.src.network.d2rl      import d2rl
-from dragonfly.src.network.conv1d    import conv1d
 from dragonfly.src.network.conv2d    import conv2d
 from dragonfly.src.network.lstm      import lstm
 from dragonfly.src.network.gated_fc  import gated_fc
@@ -15,7 +14,6 @@ net_factory = factory()
 # Register values
 net_factory.register("fc",        fc)
 net_factory.register("d2rl",      d2rl)
-net_factory.register("conv1d",    conv1d)
 net_factory.register("conv2d",    conv2d)
 net_factory.register("lstm",      lstm)
 net_factory.register("gated_fc",  gated_fc)
