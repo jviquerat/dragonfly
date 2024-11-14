e64ae0823685a05bc740c733da9a951d8e12c840
Jonathan <jonathan.viquerat@mines-paristech.fr>
2024-11-14 16:07:14 +0100

    srl: removed

--- dragonfly/src/agent/agent.py
+++ dragonfly/src/agent/agent.py
@@ -2,7 +2,6 @@
 from dragonfly.src.core.factory   import factory
 from dragonfly.src.agent.a2c      import a2c
 from dragonfly.src.agent.ppo      import ppo
-from dragonfly.src.agent.ppo_srl  import ppo_srl
 from dragonfly.src.agent.dqn      import dqn
 from dragonfly.src.agent.ddpg     import ddpg
 from dragonfly.src.agent.td3      import td3
@@ -15,7 +14,6 @@ agent_factory = factory()
 # Register agents
 agent_factory.register("a2c",      a2c)
 agent_factory.register("ppo",      ppo)
-agent_factory.register("ppo-srl",  ppo_srl)
 agent_factory.register("dqn",      dqn)
 agent_factory.register("ddpg",     ddpg)
 agent_factory.register("td3",      td3)
