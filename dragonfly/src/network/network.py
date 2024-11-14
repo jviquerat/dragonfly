f03703f60860615b5244e985361eb823600f5607
Jonathan <jonathan.viquerat@mines-paristech.fr>
2024-11-14 16:16:38 +0100

    network: cleaned unused classes

--- dragonfly/src/network/network.py
+++ dragonfly/src/network/network.py
@@ -1,21 +1,13 @@
 # Custom imports
-from dragonfly.src.core.factory      import factory
-from dragonfly.src.network.fc        import fc
-from dragonfly.src.network.d2rl      import d2rl
-from dragonfly.src.network.conv2d    import conv2d
-from dragonfly.src.network.lstm      import lstm
-from dragonfly.src.network.gated_fc  import gated_fc
-from dragonfly.src.network.ae        import ae
-from dragonfly.src.network.conv2d_ae import *
+from dragonfly.src.core.factory   import factory
+from dragonfly.src.network.fc     import fc
+from dragonfly.src.network.d2rl   import d2rl
+from dragonfly.src.network.conv2d import conv2d
 
 # Declare factory
 net_factory = factory()
 
 # Register values
-net_factory.register("fc",        fc)
-net_factory.register("d2rl",      d2rl)
-net_factory.register("conv2d",    conv2d)
-net_factory.register("lstm",      lstm)
-net_factory.register("gated_fc",  gated_fc)
-net_factory.register("ae",        ae)
-net_factory.register("conv2d_ae", conv2d_ae)
+net_factory.register("fc",     fc)
+net_factory.register("d2rl",   d2rl)
+net_factory.register("conv2d", conv2d)
