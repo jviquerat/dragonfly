5afdd99db24d2896d9fb01707f4083ae639dcb7b
jviquerat <jonathan.viquerat@mines-paristech.fr>
2024-12-11 20:53:18 +0100

    benchmarks: modified job.sh

--- dragonfly/env/benchmarks/job.sh
+++ dragonfly/env/benchmarks/job.sh
@@ -2,15 +2,18 @@
 #
 #SBATCH --job-name=dgf_benchmark
 #SBATCH --output=dgf_benchmark.txt
-#SBATCH --partition=main
+#SBATCH --partition=MAIN
+#SBATCH --qos=calcul
 #
 #SBATCH --nodes 1
-#SBATCH --ntasks 12
-#SBATCH --ntasks-per-node 12
+#SBATCH --ntasks 64
+#SBATCH --ntasks-per-node 64
 #SBATCH --ntasks-per-core 1
 #SBATCH --time=7-00:00:00
 #
 folder=$1
 n_cpu=${2-1}
-source ~/dragonfly/venv/bin/activate
+module load cimlibxx/drl/python3.11
+module load openmpi/4.1.1
+source ~/scratch/dragonfly/venv/bin/activate
 ./bench_folder.sh $folder $n_cpu
